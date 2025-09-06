190f7fedd7b284279b66c24e579f7724d9a57721
Aakashch-code <aakashch.code@gmail.com>
2025-09-06 09:22:23 +0530

    Updated Minor Things

--- app/src/main/java/com/example/smartfinance/ui/fynix/network/FynixApiService.java
+++ app/src/main/java/com/example/smartfinance/ui/fynix/network/FynixApiService.java
@@ -8,7 +8,7 @@ import retrofit2.http.POST;
 
 public interface FynixApiService {
 
-    @POST("models/gemini-1.5-pro:generateContent")
+    @POST("models/gemini-1.5-flash:generateContent")
     Call<ApiModels.GeminiResponse> chatWithGemini(
             @Body ApiModels.GeminiRequest request
     );
